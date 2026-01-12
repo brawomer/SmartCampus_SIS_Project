a98b45e9c8d66643e8ff59c5b554a099800d1fba
rawandsq4-sys <rawandsq4@gmail.com>
2026-01-13 00:34:50 +0300

    new

---
+++ Campus-Web/smart-campus-frontend/src/App.tsx
@@ -0,0 +1,7 @@
+import TechnicianDashboard from "./pages/technician/Dashboard";
+
+function App() {
+  return <TechnicianDashboard />;
+}
+
+export default App;
--- backend_API/CampusAPI/CampusAPI/Program.cs
+++ backend_API/CampusAPI/CampusAPI/Program.cs
@@ -1,20 +1,32 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<CampusAPI.Data.AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Add services to the container.
-
+// Add services to the container
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// ✅ ADD CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy =>
+        {
+            policy
+                .WithOrigins("http://127.0.0.1:5500")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -22,6 +34,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// ✅ USE CORS (MUST be before MapControllers)
+app.UseCors("AllowFrontend");
 
 app.UseAuthorization();
 
